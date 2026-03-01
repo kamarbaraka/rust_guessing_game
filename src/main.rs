d605d6721cac7ec39e4644e9a2cf989cdcf8c5b7
samson baraka <kamar254baraka@gmail.com>
2026-03-01 16:20:21 +0300

    guessing game MVP

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("=== GUESS THE NUMBER ===");
-    let secret_number= rand::thread_rng().gen_range(1..=100);
-    println!("==> Secret number: {secret_number} <===");
-    println!("Enter your guess: ");
 
-    let mut guess: String = String::new();
+    loop {
+        let secret_number: u8 = rand::thread_rng().gen_range(1..=100);
+        //println!("==> Secret number: {secret_number} <===");
+        println!("Enter your guess: (1-100)");
 
-    io::stdin().read_line(&mut guess).expect("Invalid input!");
+        let mut guess: String = String::new();
 
-    println!("=== Your guess: {} ===", guess.trim());
+        io::stdin().read_line(&mut guess).expect("Invalid input!");
+
+        guess.retain(|c| !c.is_whitespace());
+
+        if guess == "quit" {println!("===> goodbye!");break;}
+
+        let guess: u8 = match guess.parse() {
+            Ok(num) => num,
+            Err(err) => {println!("=!= {}", err);continue;}
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("===< Too small ==="),
+            Ordering::Greater => println!("===> Too big ==="),
+            Ordering::Equal => {
+                println!("=== You WIN ===");
+                break;
+            }
+        }
+
+        println!("=== Your guess: {} ===", guess);
+    }
 }
